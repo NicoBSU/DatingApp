aac5ff6d40763df5638ff54d65f994f939dab67d
Logoshin Nikolai <lgrf53@gmail.com>
2021-08-09 10:38:29 +0300

    register client side

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -12,11 +13,13 @@ export class RegisterComponent implements OnInit {
 
   
   @Output() cancelRegister = new EventEmitter();
-  model: any = {};
   registerForm: FormGroup = {} as FormGroup;
   //maxDate:Date;
+  validationErrors:string[] = [];
+
+
   constructor(private accountService:AccountService, private toastr:ToastrService,
-    private fb: FormBuilder) { }
+    private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
     this.initializeForm();
@@ -50,14 +53,11 @@ export class RegisterComponent implements OnInit {
 
 
   register(){
-    console.log(this.registerForm.value);
-    // this.accountService.register(this.model).subscribe(response=>{
-    //   console.log(response);
-    //   this.cancel();
-    // },error =>{
-    //   console.log(error);
-    //   this.toastr.error(error.error);
-    // })
+    this.accountService.register(this.registerForm.value).subscribe(response=>{
+      this.router.navigateByUrl('/members');
+    },error =>{
+      this.validationErrors = error;
+    })
   }
 
   cancel(){
