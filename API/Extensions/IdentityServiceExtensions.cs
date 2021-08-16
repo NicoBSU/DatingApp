4e0d874b49c974a3047fd67d4865867cbfc1e7e5
Logoshin Nikolai <lgrf53@gmail.com>
2021-08-16 22:26:59 +0300

    added function to manage user roles and also a little spelling fix in policies

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -36,7 +36,7 @@ namespace API.Extensions
 
             services.AddAuthorization(opt => {
                 opt.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
-                opt.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin","Modetator"));
+                opt.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin","Moderator"));
             });
 
             return services;
