7d298be670946463c109ddfb5067a9798c4b554e
Logoshin Nikolai <lgrf53@gmail.com>
2021-08-20 00:20:17 +0300

    deleted all old migrations and created new one. switched database from sqlite to postgres. Changed connection string

--- API/Extensions/ApplicationServiceExtentions.cs
+++ API/Extensions/ApplicationServiceExtentions.cs
@@ -21,7 +21,7 @@ namespace API.Extensions
             services.AddScoped<LogUserActivity>();
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(options=>{
-                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+                options.UseNpgsql(config.GetConnectionString("DefaultConnection"));
             });
             return services;
         }
